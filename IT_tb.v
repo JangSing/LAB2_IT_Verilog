03e7ce36cc7bcfcf630df7cefe04af7119c8e8f7
JangSing <jangsing94@live.com.my>
2015-12-09 11:53:16 +0800

    modify DP so that it is able to program,reg and mux are added

--- IT_tb.v
+++ IT_tb.v
@@ -1,11 +1,13 @@
 `timescale 1ps/1ps
 
 module IT_tb();
-  reg   Enter;
+  reg   Enter,programEn,Addrload,PRload;
   reg   [7:0]Input;
+  reg   [4:0]AddrSel;
   reg   Clock,Reset;
   wire  Halt;
   wire  [7:0]Output;
+
 
 //always triggerring the clock
   initial
@@ -24,13 +26,13 @@ module IT_tb();
    
   initial 
     begin
-      repeat (10) @(negedge Clock);
-      $display("At time %0dps:\tInput=%b\tOutput=%b\n",$time,Input,Output);
+      repeat (2) @(negedge Clock);programEn=1;
+      $display("At time %0dps:\tInput=%b\tOutput=%b\tprogramEn=%b\n",$time,Input,Output,programEn);
       
       
       
     end
    
-  IT IT_test(Enter,Input,Clock,Reset,Halt,Output); 
+  IT IT_test(Enter,programEn,Addrload,PRload,Input,AddrSel,Clock,Reset,Halt,Output); 
    
 endmodule
